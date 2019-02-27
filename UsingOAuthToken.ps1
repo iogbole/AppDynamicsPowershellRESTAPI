2d6b9e9f9b20f517d92757c343585cb47fcbc9a1
Israel Ogbole <iogbole@yahoo.com>
2019-02-27 16:34:04 +0000

    Create UsingOAuthToken.ps1
    Proxy and API Client example

---
+++ UsingOAuthToken.ps1
@@ -0,0 +1,24 @@
+
+
+system.net.webrequest]::defaultwebproxy = new-object system.net.webproxy('proxyHost:ProxyPort')
+
+[system.net.webrequest]::defaultwebproxy.credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
+
+[system.net.webrequest]::defaultwebproxy.BypassProxyOnLocal = $true
+
+
+$hostname = "https://customername.saas.appdynamics.com"
+
+$endpoint = "/controller/rest/applications?output=json"
+
+$url = $hostname+$endpoint
+
+Write-Host "Connecting to URL: $url"
+
+$token = "Bearer paste-token-here "
+
+$headers = @{ Authorization = $token }
+
+$result = Invoke-WebRequest -uri $url -Headers $headers
+
+Write-Host $result
